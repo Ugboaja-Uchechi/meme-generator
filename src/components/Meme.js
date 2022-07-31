83c51c70eddfb3dd9a1b4d5865f8f48bd335e5c4
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-07-31 15:13:17 +0100

    Create meme component and style it

---
+++ src/components/Meme.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Meme = () => {
+  return (
+    <main>
+      <form className="form">
+        <input type="text" className="form-input" />
+        <input type="text" className="form-input" />
+        <button className="form-button">Generate a meme image</button>
+      </form>
+    </main>
+    )
+}
+
+export default Meme;
